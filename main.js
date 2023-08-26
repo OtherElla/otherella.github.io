f2405e7f5617bb32e8cf27ea360f5c10abb04b54
Ella Hellum <93057197+OtherElla@users.noreply.github.com>
2023-08-26 21:05:00 +0000

    STUFF

---
+++ main.js
@@ -0,0 +1,12 @@
+function openGosh() {
+  window.open("https://github.com/gosh-terminal/gosh");
+}
+function goToWork() {
+  window.location.replace("/work");
+}
+function goToHome() {
+  window.location.replace("/");
+}
+function goToSkills() {
+  window.location.replace("/skills");
+}
